ac5122db75322f4e2ccda07968d8a31ad1f58d35
BerryRock0 <mdenials128@gmail.com>
2026-05-19 11:29:07 +0000

    Update EtherPlayerEditor.lua

--- src/main/resources/EtherHack/lua/components/panels/EtherPlayerEditor.lua
+++ src/main/resources/EtherHack/lua/components/panels/EtherPlayerEditor.lua
@@ -150,12 +150,13 @@ function EtherPlayerEditor:createChildren()
     local nutrition = self.localPlayer and self.localPlayer:getNutrition()
     local weight = nutrition and tostring(math.floor(nutrition:getWeight())) or "0"
     local calories = nutrition and tostring(math.floor(nutrition:getCalories())) or "0"
-    
+
     self:addLabel(getText("IGUI_char_Survived_For").. ": " .. timeSurvived, 90, 110);
     local editTimeBtn = ISButton:new(250, 110, 60, 18, getTranslate("UI_PlayerEditor_EditStats"), self, self.onEditTimeButton)
     editTimeBtn:initialise()
     editTimeBtn:instantiate()
     self:addChild(editTimeBtn)
+	
     self:addLabel(getText("IGUI_char_Zombies_Killed").. ": " .. tostring(zombieKills), 90, 130);
     local editKillsBtn = ISButton:new(250, 130, 60, 18, getTranslate("UI_PlayerEditor_EditStats"), self, self.onEditKillsButton)
     editKillsBtn:initialise()
@@ -163,6 +164,11 @@ function EtherPlayerEditor:createChildren()
     self:addChild(editKillsBtn)
 
     self:addLabel(getText("IGUI_PlayerStats_AccessLevel") .. " ".. accessLevel, 300, 10);
+    local editAccessBtn = ISButton:new(250, 110, 60, 18, getTranslate("UI_PlayerEditor_EditAccess"), self, self.onEditAccessButton)
+    editTimeBtn:initialise()
+    editTimeBtn:instantiate()
+    self:addChild(editAccessBtn)
+
     self:addLabel(getText("IGUI_PlayerStats_ChatMuted").. " ".. chatMuted, 300, 30);
     self:addLabel(getText("IGUI_char_Weight").. ": ".. weight, 300, 50);
     self:addLabel(getTranslate("UI_PlayerEditor_PlayerInfo_Calories").. ": ".. calories, 300, 70);
@@ -192,6 +198,23 @@ function EtherPlayerEditor:updateLabels()
     end
     -- Recreate all elements
     self:createChildren()
+end
+
+function EtherPlayerEditor:onEditAccessButton()
+	local modal = ISTextBox:new(0, 0, 280, 180, getTranslate("UI_PlayerEditor_EditAccessTitle"),
+		tostring(getAccessLevel()),
+        self,
+		function(target, button)
+			if button.internal == "OK" then
+				local value = tostring(button.parent.entry:getText())
+				if value then
+					setAccesslevel(value)
+					self:updateLabels()
+				end
+			end
+		end)
+	modal:initialise()
+    modal:addToUIManager()
 end
 
 function EtherPlayerEditor:onEditTimeButton()
