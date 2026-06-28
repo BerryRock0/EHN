0db5e76a49e5c07842f6e790578b615c197ac64e
BerryRock0 <mdenials128@gmail.com>
2026-06-28 17:49:25 +0000

    Update EtherDebugMenu.lua

--- src/main/resources/EtherHack/lua/components/override/EtherDebugMenu.lua
+++ src/main/resources/EtherHack/lua/components/override/EtherDebugMenu.lua
@@ -223,9 +223,9 @@ if EtherDebugMenu.tab == "DEV" then self:onClick_Dev() end
 if EtherDebugMenu.tab == "CHEATS" then self:onClick_Cheats() end
 end
 
-function EtherDebugMenu:onClick_Main() EtherDebugMenu.tab = "MAIN" self.visibility() self:setHeight(self.mainTab._y+10); end
-function EtherDebugMenu:onClick_Dev() EtherDebugMenu.tab = "DEV" self.visibility() self:setHeight(self.devTab._y+10); end
-function EtherDebugMenu:onClick_Cheats() EtherDebugMenu.tab = "CHEATS" self.visibility() self:setHeight(self.cheatsTab._y+10); end
+function EtherDebugMenu:onClick_Main() EtherDebugMenu.tab = "MAIN" self:visibility() self:setHeight(self.mainTab._y+10); end
+function EtherDebugMenu:onClick_Dev() EtherDebugMenu.tab = "DEV" self:visibility() self:setHeight(self.devTab._y+10); end
+function EtherDebugMenu:onClick_Cheats() EtherDebugMenu.tab = "CHEATS" self:visibility() self:setHeight(self.cheatsTab._y+10); end
 
 function EtherDebugMenu:visibility()
     for _, b in ipairs(self.mainTab._buttons) do b:setVisible(EtherDebugMenu.tab == "MAIN") end
