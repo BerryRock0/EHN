9ed05d30781892a58a3df082b65dd066c3395583
BerryRock0 <mdenials128@gmail.com>
2026-09-12 07:44:01 +0000

    Update EtherAPI.java

--- src/main/java/EtherHack/Ether/EtherAPI.java
+++ src/main/java/EtherHack/Ether/EtherAPI.java
@@ -199,7 +199,7 @@ public class EtherAPI {
    
    private void bypassDebugMode()
    {
-      Core.debug = this.isBypassDebugMode && GameClient.bIngame;
+      Core.debug = this.isBypassDebugMode;
    }
 
    @SubscribeLuaEvent(eventName = "OnRenderTick")
